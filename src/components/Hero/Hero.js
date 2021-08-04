00457397022b81a4aa057d7f6ef0d22d80defabf
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-08-03 21:34:06 -0400

    add link and change font color

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -18,6 +18,15 @@ const Hero = (props) => (
 				I'm Chenguang Xu, a software engineer in Brooklyn, NY. <br />I enjoy
 				making complex problems simple, elegant and crafting clean &
 				user-friendly experiences.
+				<br /> <br />
+				You can find my resume{' '}
+				<a
+					href='https://drive.google.com/file/d/1m8--nC-XmY5MCMaOpKgR1oTKEfbvuczH/view?usp=sharing'
+					target='_blank'
+					style={{ color: 'white' }}
+				>
+					here.
+				</a>
 			</SectionText>
 		</LeftSection>
 	</Section>
--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -76,7 +76,11 @@ const Timeline = () => {
 				joined 100Devs community, built different projects and had the chance
 				the work with other engineers. <br />
 				<br /> I've also been serving the US Army Reserve as a{' '}
-				<a href='https://photos.app.goo.gl/Ehb3BGf3tRy9Xw6f6' target='_blank'>
+				<a
+					href='https://photos.app.goo.gl/Ehb3BGf3tRy9Xw6f6'
+					target='_blank'
+					style={{ color: 'cyan' }}
+				>
 					vehicle mechanic{' '}
 				</a>
 				since 2015.

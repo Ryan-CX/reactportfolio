bcd49988e41020ade4b3d7aff549f90264238088
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-08-21 18:52:34 -0400

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -45,8 +45,12 @@ const Projects = () => (
 							</TagList>
 						</div>
 						<UtilityList>
-							<ExternalLinks href={visit}>Visit</ExternalLinks>
-							<ExternalLinks href={source}>Source</ExternalLinks>
+							<ExternalLinks href={visit} target='_blank'>
+								Visit
+							</ExternalLinks>
+							<ExternalLinks href={source} target='_blank'>
+								Source
+							</ExternalLinks>
 						</UtilityList>
 					</BlogCard>
 				)
